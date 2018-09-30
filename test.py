992f0dded1ef87da759d3950b92a2cfa632136df
Sunghee Jung <hash2430@gmail.com>
2018-09-30 16:39:54 +0900

    Category: feature add - 테스트 결과를 파일로 출력하는 기능 추가

--- test.py
+++ test.py
@@ -2,9 +2,12 @@ from korean import normalize
 
 # read from test.txt and run normalize()
 file = open('./test.txt', 'r')
+wFile = open('./testResult.txt', 'w')
 while(True):
     line = file.readline();
     outLine = normalize(line);
     print(outLine)
+    wFile.write(outLine)
+    wFile.write("\n")
     if (not line):
         break
