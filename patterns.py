e8061166aace0ad7d190bd7ac12334f063c38e37
Sunghee Jung <hash2430@gmail.com>
2018-09-30 16:39:06 +0900

    Category: productivity - test cases added

--- patterns.py
+++ patterns.py
@@ -1,9 +1,9 @@
 #-*- coding: utf-8 -*-
 
-eng_unit_pattern = "[ ]?(cm|mm|m|km|g|kg|Hz|KHz|GHz)"
+eng_unit_pattern = "[ ]?(cm|mm|m|km|g|kg|Hz|kHz|GHz)"
 other_unit_pattern = "[ ]?(%|℃|㎓|㎡)"
 number_pattern = "([+-]?\d[\d,]*)[\.]?\d*"
 count_pattern = "[ ]?(가지|마리|송이|곳)"
 quote_pattern = """([`"'＂“‘])(.+?)([`"'＂”’])"""
 alphabet_pattern = '[A-Z]+'
-chinese_pattern = '(中|日|北|美|故|佛)'
+chinese_pattern = '(中|日|北|美|故|佛|愛)'
